10774edb4369c5b96e1b33af56c51d002d88abbb
OdairVasconcellos <124107318+OdairVasconcellos@users.noreply.github.com>
2023-02-14 21:42:48 -0300

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,28 @@
+
+function contagem() {
+
+    var c = 10;
+    var texto;
+    var resposta = document.getElementById('contagem');
+    while (c >= 0) {
+
+        texto = c;
+        resposta.innerHTML += texto + "<br />";
+        c--;
+
+    }
+
+}
+
+function tabuada() 
+
+    var c = 1*5;
+    var texto;
+    var resposta = document.getElementById('tabuada');
+    while (c < 5*10) {
+
+        texto = c;
+        resposta.innerHTML += texto + "<br />";
+        c--;
+
+    }
